1db81e510b53270463b34d2f33596499ead70b4c
zenghui <410486047@qq.com>
2020-08-05 23:32:40 +0800

    temp commit

---
+++ src/test/java/org/spring/test/v5/PointcutTest.java
@@ -0,0 +1,41 @@
+package org.spring.test.v5;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author zenghui
+ * 2020/8/5
+ */
+public class PointcutTest {
+    /*@Test
+    public void testPointcut() throws Exception{
+
+        String expression = "execution(* org.litespring.service.v5.*.placeOrder(..))";
+
+        AspectJExpressionPointcut pc = new AspectJExpressionPointcut();
+        pc.setExpression(expression);
+
+        MethodMatcher mm = pc.getMethodMatcher();
+
+        {
+            Class<?> targetClass = PetStoreService.class;
+
+            Method method1 = targetClass.getMethod("placeOrder");
+            Assert.assertTrue(mm.matches(method1));
+
+            Method method2 = targetClass.getMethod("getAccountDao");
+            Assert.assertFalse(mm.matches(method2));
+        }
+
+        {
+            Class<?> targetClass = org.litespring.service.v4.PetStoreService.class;
+
+            Method method = targetClass.getMethod("getAccountDao");
+            Assert.assertFalse(mm.matches(method));
+        }
+
+    }*/
+}
